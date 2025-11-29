063552f969d417dd4deeb310b7eebdcb803b8971
Gustavo Marques <gugamello2014@yahoo.com>
2025-11-29 10:05:19 -0300

    feat: enhance markdownToHtml to add target="_blank" to links for improved security

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,13 @@
+'use server';
 import { remark } from 'remark';
 import html from 'remark-html';
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark().use(html).process(markdown);
-  return result.toString();
+  const content = result.toString();
+  return addTargetBlank(content);
+}
+
+function addTargetBlank(html: string) {
+  return html.replace(/<a /g, '<a target="_blank" rel="noreferrer" ');
 }
